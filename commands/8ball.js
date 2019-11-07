f3dea0d1e504e3de22204fc57f1d39189fb3ecb0
Fyrlex <31144090+Fyrlex@users.noreply.github.com>
2019-11-07 10:34:16 -0500

    new responses
    if you don't use a question mark you get a special response

--- commands/8ball.js
+++ commands/8ball.js
@@ -10,52 +10,65 @@ module.exports.run = async (bot, message, args) => {
     // if (message.author.id !== "292821168833036288") return message.reply(maintenance);
 
     let ev = botconfig.eversion;
-  
+
     let needperm = "I need the permission `Manage Messages/Embed Links` to send my messages and delete your message!"
 
     if (!message.guild.me.hasPermission("EMBED_LINKS")) return message.reply(needperm).then(msg => {
         msg.delete(10000)
     })
 
-    let noquestionmark = new Discord.RichEmbed()
-        .setColor("#ff0000")    
-        .setDescription("**ERROR:** Please ask a full question with `?` at the end!")
-        .setTimestamp()
+    if (message.content.endsWith("?")) {
 
-    if (!message.content.endsWith("?")) return message.channel.send(noquestionmark).then(msg => {
-        msg.delete(10000)
-    })
+        let replynumber = EnmapRepliesDb.get(`${message.guild.id}`, "replynumber")
+        let replies;
 
-    let replynumber = EnmapRepliesDb.get(`${message.guild.id}`, "replynumber")
-    let replies;
-    if (replynumber === 1) {
-        replies = botconfig.all
+        if (replynumber === 1) {
+            replies = botconfig.all
+        }
+        if (replynumber === 2) {
+            replies = botconfig.clean
+        }
+        if (replynumber === 3) {
+            replies = botconfig.explicit
+        }
+
+        let botname = bot.user.username;
+        let result = Math.floor((Math.random() * replies.length));
+        let question = args.slice(0).join(" ");
+        let answer = replies[result];
+
+        let ballEmbed = new Discord.RichEmbed()
+
+            .setColor("#9a00ff")
+            .setAuthor(`${botname} - 8Ball ${ev}`, bot.user.displayAvatarURL)
+            .setDescription(`Asked by ${message.author.tag}`)
+            .addField("Question", question)
+            .addField("Answer", answer)
+            .setTimestamp()
+            .setFooter("Join support @ discord.gg/MCRbYdc - Magic8", bot.user.displayAvatarURL)
+
+        message.channel.send(ballEmbed)
+
+    } else {
+
+        let replies = botconfig.noquestionmark;
+        let result = Math.floor((Math.random() * replies.length))
+        let question = args.slice(0).join(" ");
+        let answer = replies[result]
+
+        let ballEmbed = new Discord.RichEmbed()
+
+            .setColor("#9a00ff")
+            .setAuthor(`${botname} - 8Ball ${ev}`, bot.user.displayAvatarURL)
+            .setDescription(`Asked by ${message.author.tag}`)
+            .addField("Question", question)
+            .addField("Answer", answer)
+            .setTimestamp()
+            .setFooter("Join support @ discord.gg/MCRbYdc - Magic8", bot.user.displayAvatarURL)
+
+        message.channel.send(ballEmbed)
     }
-    if (replynumber === 2) {
-        replies = botconfig.clean
-    }
-    if (replynumber === 3) {
-        replies = botconfig.explicit
-    }
-
-    let botname = bot.user.username;
-
-    let result = Math.floor((Math.random() * replies.length));
-    let question = args.slice(0).join(" ");
-    let answer = replies[result];
-
-    let ballEmbed = new Discord.RichEmbed()
-
-        .setColor("#9a00ff")
-        .setAuthor(`${botname} - 8Ball ${ev}`, bot.user.displayAvatarURL)
-        .setDescription(`Asked by ${message.author.tag}`)
-        .addField("Question", question)
-        .addField("Answer", answer)
-        .setTimestamp()
-        .setFooter("Join support @ discord.gg/MCRbYdc - Magic8", bot.user.displayAvatarURL)
-
-    message.channel.send(ballEmbed)
-
+    
     let bots = message.guild.members.filter(member => member.user.bot).size;
     let users = message.guild.members.filter(member => !member.user.bot).size;
 
