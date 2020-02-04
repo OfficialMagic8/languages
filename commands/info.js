040b591d300fdeccf19d1950423ac54878d5ba93
Fyrlex <31144090+Fyrlex@users.noreply.github.com>
2020-02-03 20:42:17 -0500

    Update info.js

--- commands/info.js
+++ commands/info.js
@@ -72,7 +72,7 @@ module.exports.run = async (bot, message, args) => {
 
         .setColor("#9a00ff")
         .setAuthor(`${botname} - Information`, bot.user.displayAvatarURL)
-        .setDescription(`\n\n**__Bot Stats__**\n**Total Guilds:** ${bot.guilds.size}\n**Total Users:** ${users}\n**Total Channels:** ${channels}\n\n**8ball Plays:** ${totaluses}**\n\n__Server Stats__**\n**Bots/Users/Online:** ${sbots}/${susers}/${online}\n**Text/Voice Channels:** ${tchannels}/${vchannels}\n\n__**Channels**__\n**8ball:** ${eabrackethashtag}${echannelname}${eabracket}\n\n**Reply Type:** ${replytype}`)
+        .setDescription(`\n\n**__Bot Stats__**\n**Total Guilds:** ${bot.guilds.size}\n**Total Users:** ${users}\n**Total Channels:** ${channels}\n\n**8ball Plays:** ${totaluses}**\n\n__Server Stats__**\n**Bots/Users/Online:** ${sbots}/${susers}/${online}\n**Text/Voice Channels:** ${tchannels}/${vchannels}\n\n**8ball Channel:** ${eabrackethashtag}${echannelname}${eabracket}\n**Reply Type:** ${replytype}`)
         .addField("Ping", `${Date.now() - message.createdTimestamp}ms`)
         .addField("Donate", "__https://www.paypal.me/magic8bot__")
         .addField("Source Code", "__https://github.com/Fyrlex/Magic8__", true)
